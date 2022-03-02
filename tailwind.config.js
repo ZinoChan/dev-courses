afa058181037b3ca6438fcdd831ca07163167ff1
Zineb ELGUIL <devzinochan@gmail.com>
2022-03-02 17:47:12 +0000

    add fonts & colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: ["Roboto Slab", ...defaultTheme.fontFamily.mono],
+      secondary: ["Roboto", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        "primary-100": "#E33761",
+        "primary-200": "#D903FF",
+        "primary-300": "#8500FF",
+        "primary-dark-100": "#20267A",
+        "primary-dark-200": "#2B193D",
+      },
+    },
   },
   plugins: [],
 };
