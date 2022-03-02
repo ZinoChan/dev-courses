05e7bce97e37c379e15675cd929afde7c2532771
Zineb ELGUIL <devzinochan@gmail.com>
2022-03-02 18:50:58 +0000

    add navbar component

---
+++ src/common/components/UI/Navbar.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  const navLinks = [
+    { name: "Home", link: "/" },
+    { name: "Courses", link: "/" },
+    { name: "Pricing", link: "/" },
+    { name: "FAQ", link: "/" },
+  ];
+
+  return (
+    <header>
+      <nav className="max-w-screen-lg mx-auto p-2 flex items-center justify-between">
+        <div>
+          <h1 className="text-xl font-primary font-bold uppercase text-primary-100">
+            logo
+          </h1>
+        </div>
+        <ul className="flex space-x-4 items-center">
+          {navLinks.map(({ name, link }, index) => (
+            <li key={`${name}-${index}`}>
+              <Link href={link}>
+                <a className="text-primary-dark-200 capitalize font-bold hover:text-primary-200">
+                  {name}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <button className="rounded-full px-4 py-1 border border-primary-dark-200 text-primary-dark-200">
+          Login
+        </button>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
