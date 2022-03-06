f800fca465332504cdc56849eb32e91bbe341227
Zineb ELGUIL <devzinochan@gmail.com>
2022-03-06 20:52:11 +0000

    added fake data

---
+++ fakeData.ts
@@ -0,0 +1,27 @@
+import food from "./src/assets/food.jpg";
+import movie from "./src/assets/movie.jpg";
+
+export const fakeData = [
+  {
+    id: "1a",
+    courseImg: food,
+    courseTitle: "React js food delevery app",
+    description: ` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae
+          omnis quas quaerat obcaecati, doloribus architecto illo quibusdam
+          minima`,
+    courseDuration: 29,
+    numberOfVideos: 45,
+    rating: 4,
+  },
+  {
+    id: "2a",
+    courseImg: movie,
+    courseTitle: "Next js anime movies app",
+    description: ` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae
+          omnis quas quaerat obcaecati, doloribus architecto illo quibusdam
+          minima`,
+    courseDuration: 19,
+    numberOfVideos: 35,
+    rating: 4,
+  },
+];
