64b317828bed56aeae8ec8d01af957a0f2361014
Zineb ELGUIL <devzinochan@gmail.com>
2022-03-14 17:22:24 +0000

    add fake reviews

--- fakeData.ts
+++ fakeData.ts
@@ -25,3 +25,51 @@ export const fakeData = [
     rating: 4,
   },
 ];
+
+export const reviewsSimple = [
+  {
+    id: "1aa",
+    user_image: "",
+    user_name: "Viezh Robert",
+    user_location: "casablanca, Morocco",
+    rating: 4.5,
+    comment: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore
+    quam, ullam aspernatur fuga officia rerum et dolorum at`,
+  },
+  {
+    id: "2aa",
+    user_image: "",
+    user_name: "Danial Oz",
+    user_location: "london, UK",
+    rating: 4.3,
+    comment: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore
+    quam, ullam aspernatur fuga officia rerum et dolorum at`,
+  },
+  {
+    id: "3aa",
+    user_image: "",
+    user_name: "Samanta Rayn",
+    user_location: "madrid, Spain",
+    rating: 4.1,
+    comment: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore
+    quam, ullam aspernatur fuga officia rerum et dolorum at`,
+  },
+  {
+    id: "4aa",
+    user_image: "",
+    user_name: "Bojie ousama",
+    user_location: "tokyo, Japan",
+    rating: 4.9,
+    comment: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore
+    quam, ullam aspernatur fuga officia rerum et dolorum at`,
+  },
+  {
+    id: "5aa",
+    user_image: "",
+    user_name: "Samy abdo",
+    user_location: "Cairo, Egypt",
+    rating: 4.5,
+    comment: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore
+    quam, ullam aspernatur fuga officia rerum et dolorum at`,
+  },
+];
