42d728bc53a8cc5bcf36bc59b4f67137641094ce
Zineb ELGUIL <devzinochan@gmail.com>
2022-03-17 20:50:06 +0000

    add btn component

--- src/common/components/UI/Button.tsx
+++ src/common/components/UI/Button.tsx
@@ -1,5 +1,15 @@
-const Button = () => {
-  return <div>Button</div>;
+type Props = {
+  children?: React.ReactNode;
+  className: string;
+  onClick?: () => void;
+};
+
+const Button = ({ children, className, onClick }: Props) => {
+  return (
+    <button className={`btn ${className}`} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
